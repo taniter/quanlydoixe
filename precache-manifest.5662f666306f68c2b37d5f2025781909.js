801d099279e4aead7c14892ddc77d7cb593265f2
Nguyen Thanh Tan <thanhtanit.cntt@gmail.com>
2020-10-26 10:11:20 +0700

    Updates

---
+++ precache-manifest.5662f666306f68c2b37d5f2025781909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87e8b926e5fb4ab307d5e2096812212",
+    "url": "/quanlydoixe/index.html"
+  },
+  {
+    "revision": "539c0236e7a05b899ecf",
+    "url": "/quanlydoixe/static/css/main.14727fa6.chunk.css"
+  },
+  {
+    "revision": "cb705da571a7a128974a",
+    "url": "/quanlydoixe/static/js/2.2b0257c0.chunk.js"
+  },
+  {
+    "revision": "4712edae9788d237216439aee9b2a386",
+    "url": "/quanlydoixe/static/js/2.2b0257c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539c0236e7a05b899ecf",
+    "url": "/quanlydoixe/static/js/main.1e230cb9.chunk.js"
+  },
+  {
+    "revision": "d1eb8ff758c42ed9114e",
+    "url": "/quanlydoixe/static/js/runtime-main.ff374562.js"
+  }
+]);
